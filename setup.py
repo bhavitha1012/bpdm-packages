a75c8b32f17930ad94546973cbff4323854c9ecb
bhavitha1012 <55327840+bhavitha1012@users.noreply.github.com>
2022-10-19 11:04:53 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="bpdmpackage",
+version="0.1",
+description="This is a package",
+Long_description="this is a package",
+author="Mona",
+packages=['bpdmpackage'],
+install_requires=[])
