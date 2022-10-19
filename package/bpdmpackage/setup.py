0a623fded26c39007c5a6c29aff5c339bfc10291
bhavitha1012 <55327840+bhavitha1012@users.noreply.github.com>
2022-10-19 11:04:27 +0530

    Delete setup.py

--- package/bpdmpackage/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-setup(name="bpdmpackage",
-version="0.1",
-description="This is a package",
-Long_description="this is a package",
-author="Mona",
-packages=['bpdmpackage'],
-install_requires=[])
